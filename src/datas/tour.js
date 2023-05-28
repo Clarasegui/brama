5da86659f8047541ab6395f94123e16c6f274c69
Clara Segui <clarasegui@macbook-air.home>
2023-05-28 15:04:00 +0200

    Show datas

---
+++ src/datas/tour.js
@@ -0,0 +1,54 @@
+const tourInfos = [
+{
+    month: "JUIN",
+    concerts: [
+    {date:"2",
+    city:"Nîmes",
+    location:"Total Festum 2023",
+    }
+    ]
+},
+{
+    month: "JUILLET",
+    concerts: [
+    {date:"7/8",
+    city:"Saint-Denis-de-Gastines",
+    location:"Au Foin de La Rue",
+    },
+    {date:"14/15",
+    city:"Taix",
+    location:"Les Pieds Dans La Bassine",
+    },
+    {date:"1/12",
+    city:"Canada",
+    },
+    {date:"14",
+    city:"Carhaix",
+    location:"Vieilles Charrues",
+    },
+    {date:"21/22/23",
+    city:"Val d'Ajol",
+    location:"Le Pied Orange",
+    },
+    {date:"23",
+    city:"SP Vigo SP",
+    location:"Sinsal Festival",
+    },
+    {date:"25",
+    city:"PT Sines",
+    location:"FMM Sines",
+    },
+    ]
+},
+{
+    month: "AOÛT",
+    concerts: [
+    {date:"25",
+    city:"Château de Lafauche",
+    location:"Caph’ARTS’Naüm",
+    }
+    ]
+},
+]
+
+export default tourInfos;
--- src/pages/tour.jsx
+++ src/pages/tour.jsx
@@ -8,56 +8,6 @@ function Tour() {
                 <Navbar navBarClassName="tour_navbar" logoClassName="tour_logo" />
             </header>
             <div className="tour_content">
-                <p className="tour_content_title">MARS</p>
-                <ul className="tour_list">
-                    <li>
-                        <p className="tour_date">16</p>|<p className="tour_city">Chambery</p>|<p className="tour_location">Apej</p></li>
-                    <li>
-                        <p className="tour_date">17</p>|<p className="tour_city">Macon</p>|<p className="tour_location">La cave à Musique</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">31</p>|<p className="tour_city">Puy en Velay</p>|<p className="tour_location">Lo Festanal</p>
-                    </li>
-                </ul>
-                <p className="tour_content_title">AVRIL</p>
-                <ul className="tour_list">
-                    <li>
-                        <p className="tour_date">13</p>|<p className="tour_city">Joue les Tours</p>|<p className="tour_location">Le Temps Machine</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">15</p>|<p className="tour_city">St Brieuc</p>|<p className="tour_location">Bonjour Minuit</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">20</p>|<p className="tour_city">St Etienne</p>|<p className="tour_location">Le Fil</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">21</p>|<p className="tour_city">Paris</p>|<p className="tour_location">La Maroquinerie</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">22</p>|<p className="tour_city">Clermont Ferrand</p>|<p className="tour_location">La Copée</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">26 ou 27</p>|<p className="tour_city">Parise</p>|<p className="tour_location">La Maroquinerie</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">30</p>|<p className="tour_city">Paris</p>|<p className="tour_location">La Maroquinerie</p>
-                    </li>
-                </ul>
-                <p className="tour_content_title">MAI</p>
-                <ul className="tour_list">
-                    <li>
-                        <p className="tour_date">4</p>|<p className="tour_city">Romans</p>|<p className="tour_location">La Cordo</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">14</p>|<p className="tour_city">Dijon</p>|<p className="tour_location">La Vapeur</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">28</p>|<p className="tour_city">Vic sur Cère</p>|<p className="tour_location"></p>
-                    </li>
-                    <li>
-                        <p className="tour_date">2</p>|<p className="tour_city">Nîmes</p>|<p className="tour_location"></p>
-                    </li>
-                </ul>
                 <p className="tour_content_title">JUIN</p>
                 <ul className="tour_list">
                     <li>
@@ -66,12 +16,6 @@ function Tour() {
                 </ul>
                 <p className="tour_content_title">JUILLET</p>
                 <ul className="tour_list">
-                    <li>
-                        <p className="tour_date">7/8</p>|<p className="tour_city">Saint-Denis-de-Gastines</p>|<p className="tour_location">Au Foin de La Rue</p>
-                    </li>
-                    <li>
-                        <p className="tour_date">14/15</p>|<p className="tour_city">Taix</p>|<p className="tour_location">Les Pieds Dans La Bassine</p>
-                    </li>
                     <li>
                         <p className="tour_date">1/12</p>|<p className="tour_city">Canada</p>|<p className="tour_location"></p>
                     </li>
@@ -93,7 +37,7 @@ function Tour() {
                     <li>
                         <p className="tour_date">25</p>|<p className="tour_city">Château de Lafauche</p>|<p className="tour_location">Caph’ARTS’Naüm</p>
                     </li>
-                   </ul>
+                </ul>
             </div>
         </div>
     );
