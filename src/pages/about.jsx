ba87678e120eb55b7a2274cc13aa569bd5f7bc03
Clara Segui <clarasegui@macbook-air.home>
2023-06-21 17:24:02 +0200

    About & tour pages update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,8 +8,8 @@ function About() {
                 <Navbar navBarClassName="about_navbar" logoClassName="about_logo" />
             </header>
             <div className="about_content_container">
-                <div>
-                    <p className="about_content_center">En s’engageant tête baissée dans les contre allées
+                <div className="about_content_center">
+                    <p>En s’engageant tête baissée dans les contre allées
                         du Massif Central, Brama a choisi l’exigence
                         impitoyable du trio pour exprimer pleinement
                         la transe d’un rock psychédélique farouche et
@@ -41,22 +41,26 @@ function About() {
                 <div className="about_content_right">
                     <div className="about_content_contacts">
                         <p className="about_info_title">CONTACTS :</p>
-                        <p className="about_info">Tourneur : Yvan Soyouz</p>
-                        <p className="about_info">yvan@soyouzmusic.com</p>
+                        <p className="about_info">Tourneur : </p>
+                        <p className="about_info_name">Yvan Soyouz</p>
+                        <p className="about_info_name">yvan@soyouzmusic.com</p>
                     </div>
                     <div className="about_content_credits">
                         <p className="about_info_title">CRÉDITS :</p>
-                        <p className="about_info">Brama : Paolo Gauthier,</p>
-                        <p className="about_info">Simon Guy,</p>
-                        <p className="about_info">Baptiste Lherbeil</p>
+                        <p className="about_info">Brama : </p>
+                        <p className="about_info_name">Paolo Gauthier</p>
+                        <p className="about_info_name">Simon Guy</p>
+                        <p className="about_info_name">Baptiste Lherbeil</p>
                         <p className="about_info">Identité visuelle :</p>
-                        <p className="about_info">Flore Chemin</p>
+                        <p className="about_info_name">Flore Chemin</p>
                         <p className="about_info">Création graphique :</p>
-                        <p className="about_info">Célia Richie</p>
+                        <p className="about_info_name">Célia Richie</p>
+                        <p className="about_info">Création web :</p>
+                        <p className="about_info_name">Clara Segui</p>
                         <p className="about_info">Photo :</p>
-                        <p className="about_info">Sylvestre NoniqueDesvergnes</p>
+                        <p className="about_info_name">Sylvestre NoniqueDesvergnes</p>
                         <p className="about_info">Typo :</p>
-                        <p className="about_info">Velvetyne Type Foundry,
+                        <p className="about_info_name">Velvetyne Type Foundry,
                             Basteleur</p>
                     </div>
                 </div>
